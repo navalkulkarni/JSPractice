476ec898f50cd3fb9bf8c153aff748c0c028af3d
Naval Kulkarni <navalkulkarni@gmail.com>
2022-09-06 07:06:37 +0530

    add event listener on check button and display message if input is empty

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+console.log('liver srver demo');
+
+
+
+const checkFunction = function checkNumber(event)
+{
+  console.log('in check number'+document.querySelector('.guess').value);
+  
+  const guessValue = Number(document.querySelector('.guess').value);
+
+  if(!guessValue){
+    document.querySelector('.message').textContent = '🛑 No Number !';
+  }
+
+}
+
+document.querySelector('.check').addEventListener('click', checkFunction);
+
